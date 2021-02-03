df16a83a4c55501c5b0bf625317faf60dc30d4b6
Delph <delph_teb@hotmail.com>
2021-02-03 02:19:32 -0500

    add mysql code

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Grilled Chicken Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chili burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Blue Cheese Burger", false);
+
