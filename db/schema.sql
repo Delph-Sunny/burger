809be2c99c24999f4ca668efe83fe92582e4fac5
Delph <delph_teb@hotmail.com>
2021-02-07 18:09:44 -0500

    clean up comments and format

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,7 @@
 -- Drops the burger_db if it already exists --
 DROP DATABASE IF EXISTS burgers_db;
-
 -- Created the DB "burgers_db" (only works on local connections)
 CREATE DATABASE burgers_db;
-
 -- Use the DB burgers_db for all the rest of the script
 USE burgers_db;
 
