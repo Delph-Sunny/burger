8c9249448833c6e46d1e86f3aa04868d3e639bcf
Delph <delph_teb@hotmail.com>
2021-02-07 17:39:51 -0500

    clean up comments

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-//Server generated
+//Server generated and Express config
 const express = require("express");
 const path = require("path");
-
-// EXPRESS CONFIGURATION
 const app = express();
+
 // Sets an initial port
 const PORT = process.env.PORT || 8080;
+
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serving static files in Express, so it will load the files that are in the public directory
 app.use(express.static(path.join(__dirname, "public")));
 
-// Set Handlebars.
+// Set Handlebars
 const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
@@ -24,10 +25,6 @@ app.use(routes);
 
 
 // LISTENER
-app.listen(PORT, (err) => {
-    if (err) {
-       console.log(err)
-   } else {
-       console.log(`Server is listening on: http://localhost:${PORT}`);
-    }
-});
+app.listen(PORT, () =>
+    console.log(`Server is listening on: http://localhost:${PORT}`)
+);
